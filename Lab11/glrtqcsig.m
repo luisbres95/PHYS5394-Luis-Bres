ddb5820e8b5aa86e643cf9e189bbc063b1dcd58e
luisbres95 <92558314+luisbres95@users.noreply.github.com>
2022-04-19 21:29:30 -0500

    Add files via upload

---
+++ Lab11/glrtqcsig.m
@@ -0,0 +1,27 @@
+function llr = glrtqcsig(dataVec,psdPosFreq,a,nSamples)
+%Luis Mario Bres Castro
+%Task 3
+addpath ../Func&data
+% This is the target 
+%% Parameters for data realization
+% Number of samples and sampling frequency.
+
+sampFreq = 1024;
+timeVec = (0:(nSamples-1))/sampFreq;
+A=1;
+%% Compute GLRT
+%Generate the unit norm signal (i.e., template). Here, the value used for
+%'A' does not matter because we are going to normalize the signal anyway.
+%Note: the GLRT here is for the unknown amplitude case, that is all other
+%signal parameters are known
+sigVec = crcbgenqcsig(timeVec,A,[a(1),a(2),a(3)]);
+%We do not need the normalization factor, just the  template vector
+[templateVec,~] = normsig4psd(sigVec,sampFreq,psdPosFreq,1);
+% Calculate inner product of data with template
+llr = innerprodpsd(dataVec,templateVec,sampFreq,psdPosFreq);
+%GLRT is its square
+llr = llr^2;
+disp(llr);
+
+
+end
