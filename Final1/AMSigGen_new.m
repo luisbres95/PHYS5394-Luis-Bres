74d7e327b592d3c7a633b40c59430e3c1dc30a20
luisbres95 <92558314+luisbres95@users.noreply.github.com>
2022-04-28 13:24:43 -0500

    Add files via upload

---
+++ Final1/AMSigGen_new.m
@@ -0,0 +1,16 @@
+function SigVec = AMSigGen_new(t,A,P)
+
+%Generate an Amplitude Modulated Sinusoid Signal
+%S=AMgenSig(time,Amp,freq0,freq1,psi0)
+% Generates an Amplitude Modulated Sinisoid Signal S. 'time' is the vector of
+% time stamps at which the samples of the signal are to be computed. 'Amp'
+% is the amplitude multiplying the signal. 'freq0' is sampling frequency.
+% 'freq1' is reference frequency. 'psi0' is the phase of the signal.
+%P is the struct variable containing all of the signal parameters.
+%Luis Mario Bres Castro, February 2022
+
+
+
+SigVec=cos(2*pi*P.f1*t).*sin(2*pi*P.f0*t+P.psi0); 
+SigVec=A*SigVec./norm(SigVec);
+end
