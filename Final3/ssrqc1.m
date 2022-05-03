aae2b2d8d329a9e18dec50644af86481ec5a3ae2
luisbres95 <92558314+luisbres95@users.noreply.github.com>
2022-05-02 19:07:30 -0500

    Add files via upload

---
+++ Final3/ssrqc1.m
@@ -0,0 +1,15 @@
+%LLR after maximizing over amplitude parameter
+function ssrVal = ssrqc1(x,params)
+    %Generate normalized quadratic chirp
+    phaseVec = x(1)*params.time + x(2)*params.time2 + x(3)*params.time3;
+    qc = sin(2*pi*phaseVec);
+    % Inner product of signal with itself
+    normSigSqrd = innerprodpsd(qc,qc,params.samplFreq,params.psdVec);
+    % Normalization factor
+    normFac = params.snr/sqrt(normSigSqrd);
+    % Normalize qc signal
+    qc = normFac*qc;
+    
+    %Compute fitness
+    ssrVal = -innerprodpsd(params.dataY,qc,params.samplFreq,params.psdVec)^2;
+end
